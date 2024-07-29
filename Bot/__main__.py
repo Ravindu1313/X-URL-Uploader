4576ca9d7214ac4c48b3ac2e073851941dd828a7
Ravindu1313 <ravigimhan13@gmail.com>
2024-07-29 12:08:31 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -20,6 +20,20 @@ async def main():
     client.logger.info(f'{client.bot.me.first_name} Started!')
     await idle()
 
+from flask import Flask
+ 
+# instance of flask application
+app = Flask(__name__)
+ 
+# home route that returns below text 
+# when root url is accessed
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+ 
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
 
 if __name__ == '__main__':
     if not os.path.isdir(client.config.DOWNLOAD_LOCATION):
